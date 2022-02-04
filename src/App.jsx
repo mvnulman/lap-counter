48e07b6cb800db6f655f0cc500dce5d70476d7f3
Marcos Vinicius <mvsouza7@gmail.com>
2022-02-04 10:41:29 +0200

    docs: basic project structured done! starting to make all the components envolved in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css'
+import React from 'react'
+
+const ShowLaps = (props) => {
+  return (
+    <p>
+      {props.laps} <br />
+      Laps
+    </p>
+  );
+}
+
+const ShowTime = (props) => {
+  return (
+    <p>
+      {props.time} <br />
+      Average time per lap
+    </p>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <button>{props.text}</button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing 1, 2, 3 ...</h1>
+    <ShowLaps laps = "12" />
+    <Button text = "+" />
+    <Button text = "-" />
+    <ShowTime time = "01:30:00" />
+    <Button text = "Start" />
+    <Button text = "Stop" />
     </div>
   )
 }
