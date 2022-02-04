9c7e6693c5469bad2e91eff7e1bb347c3c27a0e4
Marcos Vinicius <mvsouza7@gmail.com>
2022-02-04 11:08:54 +0200

    docs: useState and useEffect logic for the lap counter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
-import React from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
 
 const ShowLaps = (props) => {
   return (
@@ -8,7 +8,7 @@ const ShowLaps = (props) => {
       Laps
     </p>
   );
-}
+};
 
 const ShowTime = (props) => {
   return (
@@ -17,25 +17,40 @@ const ShowTime = (props) => {
       Average time per lap
     </p>
   );
-}
+};
 
 const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+function App() {
+  const [lapNum, setLapNum] = useState(0);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log("Called!");
+    }, 1000);
+  }, []);
+
+  const increment = () => {
+    setLapNum(lapNum + 1);
+  };
+
+  const decrement = () => {
+    setLapNum(lapNum - 1);
+  };
+
   return (
-    <button>{props.text}</button>
+    <div className="App">
+      <ShowLaps laps={lapNum} />
+      <Button text="+" onClick={increment} />
+      <Button text="-" onClick={decrement} />
+      <ShowTime time={time} />
+      <Button text="Start" />
+      <Button text="Stop" />
+    </div>
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-    <ShowLaps laps = "12" />
-    <Button text = "+" />
-    <Button text = "-" />
-    <ShowTime time = "01:30:00" />
-    <Button text = "Start" />
-    <Button text = "Stop" />
-    </div>
-  )
-}
-
-export default App
+export default App;
